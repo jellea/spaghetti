a6fa2fddd80b3a2a671afac5d996cb723f38e82f
Jelle Akkerman <jelle@jelleakkerman.nl>
2014-10-11 22:29:28 +0200

    converge nodes into one component

--- src/cljs/spaghetti/core.cljs
+++ src/cljs/spaghetti/core.cljs
@@ -9,43 +9,16 @@
 
 (defonce ctx (js/webkitAudioContext.))
 
-(defonce node-types {:OscillatorNode [:type
-                                      :frequency
-                                      :detune]
-                     :GainNode [:gain]
-                     :DelayNode [:delayTime]
-                     :PannerNode [
-                                  :panningModel
-                                  :distanceModel
-                                  :refDistance
-                                  :maxDistance
-                                  :rolloffFactor
-                                  :coneInnerAngle
-                                  :coneOuterAngle
-                                  :coneOuterGain
-                                  ]
-                     :ConvolverNode [
-                                     :buffer
-                                     :normalize]
-                     :DynamicsCompressorNode [ :threshold
-                                              :knee
-                                              :ratio
-                                              :reduction
-                                              :attack
-                                              :release]
-                     :BiquadFilterNode [:type
-                                        :frequency
-                                        :Q
-                                        :detune
-                                        :gain]
-                     :WaveShaperNode [:curve
-                                      :oversample]
-                     :AnalyserNode [:fftSize
-                                    :minDecibels
-                                    :maxDecibels
-                                    :smoothingTimeConstant
-                                    :frequencyBinCount]
-                     :AudioDestinationNode []
+(defonce node-types {:OscillatorNode [{:n :type :choises ["sine" "triangle" "square"]} :frequency :detune]
+                     :GainNode [:input :gain]
+                     :DelayNode [:input :delayTime]
+                     :AudioBufferSourceNode [:playbackRate :loop :loopStart :loopEnd :buffer]
+                     :PannerNode [:input :panningModel :distanceModel :refDistance :maxDistance :rolloffFactor :coneInnerAngle :coneOuterAngle :coneOuterGain]
+                     :ConvolverNode [:input :buffer :normalize]
+                     :DynamicsCompressorNode [:input :threshold :knee :ratio :reduction :attack :release]
+                     :BiquadFilterNode [:input :type :frequency :Q :detune :gain]
+                     :WaveShaperNode [:input :curve :oversample]
+                     :AudioDestinationNode [:input]
                      :ChannelSplitterNode []
                      :ChannelMergerNode []
                      :MediaElementAudioSourceNode []
@@ -53,19 +26,18 @@
                      :MediaStreamAudioDestinationNode [:stream]})
 
 (defonce app-state (atom {:menu {:x 0 :y 0 :visible false}
-                          :text "Hello Chestnut!"
-                          :nodes [{:type :oscillator :node (.createOscillator ctx)} {:type :filter :node (.createBiquadFilter ctx)} {:type :context :node ctx}]
-                          :wires [{:x1 300 :y1 153 :x2 400 :y2 70} {:x1 600 :y1 153 :x2 700 :y2 70}]}))
+                          :nodes [{:type :OscillatorNode :node (.createOscillator ctx)} {:type :BiquadFilterNode :node (.createBiquadFilter ctx)} {:type :AudioDestinationNode :node ctx}]
+                          :wires [{:x1 300 :y1 165 :x2 400 :y2 65} {:x1 600 :y1 165 :x2 700 :y2 65}]}))
 
 (defn add-node [app n]
-  (om/transact! app #(conj % {:type :oscillator :node (.createOscillator ctx)})))
+  (om/transact! app #(conj % {:type n :node (.createOscillator ctx)})))
 
 (defcomponent contextmenu [{:keys [menu] :as app} owner]
   (render-state [_ _]
             (html
              [:ul.contextmenu {:style {:transform "translate(0px,20px)"}}
               (for [n (keys node-types)]
-                [:li {:onClick #(add-node app n)} (str "+ " (name n))])])))
+                [:li {:onClick #(add-node app n)} (str "+ "(name n))])])))
 
 (defcomponent viewport [app owner]
   (will-mount [_])
@@ -73,84 +45,40 @@
           (html
            [:div
             (om/build contextmenu (:nodes app) {})
-            (om/build-all nodes (:nodes app) {})
-            (om/build wire-canvas app {})
-            ])))
-
-(defn toggle-contextmenu [e app]
-  (.log js/console @app)
-  (om/transact! app #(assoc % :menu {:x (.-clientX e) :y (.-clientY e) :visible true})))
+            (om/build-all audio-node (:nodes app) {})
+            (om/build wire-canvas app {})])))
 
 (defcomponent wire [{:keys [x1 x2 y1 y2]} owner]
   (render-state [_ _]
-                (html [:line {:x1 x1 :y1 y1 :x2 x2 :y2 y2 :stroke "#444" :stroke-width 1}]))
-  )
+                (html [:line {:x1 x1 :y1 y1 :x2 x2 :y2 y2 :stroke "#444" :stroke-width 1}])))
+
 (defcomponent wire-canvas [app owner]
-  (did-mount [this]
-             (.addEventListener (om/get-node owner) "click" #(prn "jo!")))
   (render [_]
-          (html [:svg.maincanvas {:onClick #(toggle-contextmenu % app)}
-                 (om/build-all wire (:wires app) {})
-          ])))
+          (html [:svg.maincanvas
+                 (om/build-all wire (:wires app) {})])))
 
-(defcomponent nodes [app owner]
-  (render [_]
-    (html
-     (cond
-      (= (:type app) :oscillator) (om/build oscillator-node app {})
-      (= (:type app) :filter) (om/build filter-node app {})
-      (= (:type app) :context) (om/build context-node app {})
-      (= (:type app) :gain) (om/build gain-node app {})))))
-
-(defcomponent context-node [app owner]
-  (did-mount [_]
-             (let [node  (om/get-node owner)
-                   scope (js/WavyJones ctx (.querySelector node ".canvas"))]
-               (.connect node scope)))
-  (render-state [_ state]
-                (html [:div.node.context
-                       [:h2 "context"]
-                       [:div.io.input "input"]
-                       [:svg.canvas]])))
-
-(defcomponent filter-node [app owner]
-  (did-mount [_]
-              (.connect (:node app) (.-destination ctx)))
-  (render-state [_ state]
-                (html [:div.node.gain
-                       [:h2 "gain"]
-                       [:div.io.input "input"]
-                       [:div.io.input "gain"]
-                       [:div.io.output "output"]
-                       [:svg.canvas]])))
-
-(defcomponent gain-node [app owner]
-  (init-state [_]
-              {})
-  (did-mount [_]
-              (.connect (:node app) (.-destination ctx)))
-  (render-state [_ state]
-                (html [:div.node.gain
-                       [:h2 "gain"]
-                       [:div.io.input "input"]
-                       [:div.io.input "gain"]
-                       [:div.io.output "output"]
-                       [:svg.canvas]])))
-
-(defcomponent oscillator-node [app owner]
-  (did-mount [this]
-      (let [node       (om/get-node owner)
-            scope      (js/WavyJones ctx (.querySelector node ".canvas"))]
-        (.start (:node app) 0)
-        (.connect (:node app) scope)
-        (.connect scope (.-destination ctx))))
+(defcomponent port [app owner]
   (render-state [_ _]
     (html
-         [:div.node.oscil
-          [:h2 "oscillator"]
-          [:div.io.input "start"]
-          [:div.io.input "frequency"]
-          [:div.io.output "output"]
+     (cond
+      (= app :output) [:div.io.output "output"]
+      (some? (:choises app)) [:div.io.input (str (name (:n app)))]
+      :default [:div.io.input (name app)]))))
+
+(defcomponent audio-node [{:keys [type io node]} owner]
+  (did-mount [this]
+      (let [div       (om/get-node owner)
+            scope     (js/WavyJones ctx (.querySelector div ".canvas"))]
+        (.start node 0)
+        #_(set! (.-value (.-frequency node)) (rand-int 400 1000))
+        (.connect node (.-destination ctx))
+        (.connect node scope)))
+  (render-state [_ _]
+    (html
+         [:div.node {:class (name type)}
+          [:h2 (name type)]
+          (om/build-all port (take 6 (vec (get-in node-types [type]))) {})
+          (om/build port :output {})
           [:svg.canvas]])))
 
 (defn main []
